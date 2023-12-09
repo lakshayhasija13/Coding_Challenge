725706861d4bc4474a50bce954ca1f285ddce436
Lakshay Hasija <142048873+lakshayhasija13@users.noreply.github.com>
2023-12-09 21:58:29 +0530

    Easy: 1_reverse_string.py

---
+++ 1_reverse_string.py
@@ -0,0 +1,11 @@
+def reverse_string(input_str):
+    return input_str[::-1]
+
+# # Example usage:
+# original_str = "Hello, World!"
+# reversed_str = reverse_string(original_str)
+# print("Original String:", original_str)
+# print("Reversed String:", reversed_str)
+
+input_str = input()
+reverse_string(input_str)
